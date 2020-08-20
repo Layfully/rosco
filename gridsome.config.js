dd470a2b01e5aa377bdf76faefc7c57cb27487b3
Layfully <adriangaborek3@gmail.com>
2020-08-20 11:54:53 +0200

    feat: create structure of offer and create component for it

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,41 +5,41 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
+  siteName: "Netlify CMS Gridsome starter template",
+  siteDescription:
+    "A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.",
 
   templates: {
-    Post: '/:title',
-    Tag: '/tag/:id'
+    Post: "/:title",
+    Offer: "/:title",
+    Tag: "/tag/:id",
   },
 
   plugins: [
     {
       // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
-        refs: {
+        typeName: "Oferta",
+        path: "content/offers/*.md",
+        /*refs: {
           // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
           tags: {
-            typeName: 'Tag',
-            create: true
-          }
-        }
-      }
-    }
+            typeName: "Tag",
+            create: true,
+          },
+        },*/
+      },
+    },
   ],
 
   transformers: {
     //Add markdown support to all file-system sources
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
-    }
-  }
-}
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: ["@gridsome/remark-prismjs"],
+    },
+  },
+};

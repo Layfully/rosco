7305652c3d6fc87c2f3844ecbcd11e98b920d78f
Adrian Gaborek <unity3d09@gmail.com>
2020-09-01 08:27:17 +0200

    Revert "Navbar"

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -2,6 +2,8 @@
   <div
     class="dropdown header__menu__link"
     :class="{'header__menu__link__active': isDropdownActive}"
+    @mouseover="isOpen = true"
+    @mouseleave="isOpen = false"
   >
     <span>{{ title }}</span>
     <svg class="dropdown__triangle" viewBox="0 0 1030 638" width="10">
@@ -9,16 +11,18 @@
         d="M1017 68L541 626q-11 12-26 12t-26-12L13 68Q-3 49 6 24.5T39 0h952q24 0 33 24.5t-7 43.5z"
       />
     </svg>
-    <div class="dropdown__menu">
-      <div
-        v-for="(item, i) in items"
-        :key="i"
-        class="dropdown__menu__item"
-        :class="{'dropdown__menu__item__active': isActive(item.node.path)}"
-      >
-        <g-link :to="item.node.path">{{ item.node.title }}</g-link>
+    <transition name="fade" appear>
+      <div class="dropdown__menu" v-if="isOpen">
+        <div
+          v-for="(item, i) in items"
+          :key="i"
+          class="dropdown__menu__item"
+          :class="{'dropdown__menu__item__active': isActive(item.node.path)}"
+        >
+          <g-link :to="item.node.path">{{ item.node.title }}</g-link>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -50,59 +54,33 @@ export default {
 <style lang="scss">
 .dropdown {
   position: relative;
-  text-align: center;
-
-  svg {
-    transition: transform var(--transition-time);
-  }
-
-  &:hover > svg {
-    transform: rotate(90deg);
-  }
-
-  &:hover > &__menu {
-    opacity: 1;
-    height: auto;
-  }
-
-  &:hover > &__menu > &__menu__item {
-    display: flex;
-  }
 
   &__triangle {
-    fill: var(--title-color);
+    fill: var(--link-color);
     margin-left: calc(var(--space) / 6);
     margin-bottom: 3px;
   }
 
   &__menu {
-    opacity: 0;
-    height: 0px;
-    transition: var(--transition-time);
+    position: absolute;
+    width: 250%;
     background-color: var(--bg-color);
+    top: 100%;
+    left: 50%;
+    transform: translateX(-50%);
     border-radius: var(--radius);
+    border: 1px solid var(--border-color);
     z-index: 100;
-
-    @include md {
-      border: 1px solid var(--border-color);
-      position: absolute;
-      transform: translateX(-50%);
-      left: 50%;
-      width: 250%;
-      top: 100%;
-    }
 
     &__item {
       background-color: var(--bg-color);
-      padding: 10px 14px;
-      display: none;
-      justify-content: center;
-      transition: background-color var(--transition-time) ease;
-
-      @include md {
-        font-size: 0.75em;
-        border-bottom: 1px solid var(--border-color);
-      }
+      padding: 5px 14px;
+      position: relative;
+      text-align: center;
+      border-bottom: 1px solid var(--border-color);
+      display: flex;
+      font-size: 0.75em;
+      transition: background-color 0.3s ease;
 
       &__active {
         background-color: var(--bg-color-dark);
@@ -113,5 +91,14 @@ export default {
       }
     }
   }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.4s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -4,8 +4,8 @@
       src="@/assets/image/rosco_logo.png"
       class="logo__image"
       alt="Rosco"
-      height="65px"
-      width="250px"
+      width="200px"
+      height="80%"
       fit="inside"
     ></g-image>
   </g-link>
@@ -19,28 +19,12 @@ query {
 }
 </static-query>
 
-<script>
-export default {
-  name: "logo",
-  methods: {
-    logoHeight() {
-      return window.matchMedia("(min-width: 600px)").matches;
-    },
-  },
-};
-</script>
-
 <style lang="scss">
 .logo {
   margin-left: 2em;
   &__image {
     vertical-align: middle;
     filter: var(--logo-filter-shadow) var(--logo-filter-brightness);
-    max-width: 50%;
-
-    @include md {
-      max-width: 100%;
-    }
   }
 }
 </style>

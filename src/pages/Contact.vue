4f66e0e1a340de5519960e8f6827fd398cac564e
Layfully <adriangaborek3@gmail.com>
2020-09-08 10:35:54 +0200

    feat: add modal after sending contact form and form validation fix: fix wrong form fields placeholder name and add column layout for browsers other than chrome.

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,111 +1,105 @@
 <template>
-  <main class="main-margin">
-    <article>
-      <div class="contact__map">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10346.79074415049!2d20.667892!3d49.5845913!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2704aba2804e3887!2sROSCO%20SERWIS!5e0!3m2!1spl!2spl!4v1599122860461!5m2!1spl!2spl"
-          width="100%"
-          height="400"
-          frameborder="0"
-          style="border:0;"
-          allowfullscreen
-          aria-hidden="false"
-          tabindex="0"
-        ></iframe>
-      </div>
+  <Layout>
+    <main class="main-margin">
+      <article>
+        <div class="contact__map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10346.79074415049!2d20.667892!3d49.5845913!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2704aba2804e3887!2sROSCO%20SERWIS!5e0!3m2!1spl!2spl!4v1599122860461!5m2!1spl!2spl"
+            width="100%"
+            height="400"
+            frameborder="0"
+            style="border:0;"
+            allowfullscreen
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe>
+        </div>
 
-      <div class="contact">
-        <section>
-          <h3 class="text-center">Dane kontaktowe</h3>
-          <p>
-            Dziękujemy za zainteresowanie ofertą Rosco Serwis.
-            <br />Aby uzyskać więcej informacji na temat naszej firmy zapraszamy
-            do kontaktu z nami codziennie od poniedziałku do piątku w godzinach
-            od 7:00 do 15:00
-          </p>
-          <p
-            class="footer__contact"
-            v-for="(contact_detail, i) in $static.contact_data.contact_details"
-            :key="'contact-' + i"
-            v-html="contact_detail.contact"
-          ></p>
-        </section>
-        <section>
-          <form class="contact__form">
-            <h3 class="text-center">Napisz do nas!</h3>
-            <fieldset class="contact__form__fieldset">
-              <legend class="contact__form__fieldset__legend">
-                Formularz kontaktowy
-              </legend>
-              <label
-                class="contact__form__fieldset__label-required"
-                for="sender"
-                >Imię i nazwisko</label
-              >
-              <input
-                class="contact__form__fieldset__input"
-                v-model="formData.sender"
-                id="sender"
-                name="sender"
-                type="text"
-                placeholder="Podaj imię i nazwisko"
-                required
-              />
-              <label class="contact__form__fieldset__label-required" for="email"
-                >Adres e-mail</label
-              >
-              <input
-                class="contact__form__fieldset__input"
-                v-model="formData.email"
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Podaj email"
-                required
-              />
-              <label
-                class="contact__form__fieldset__label-required"
-                for="emailConfirmation"
-                >Potwierdź email</label
-              >
-              <input
-                class="contact__form__fieldset__input"
-                v-model="formData.emailConfirmation"
-                id="emailConfirmation"
-                name="emailConfirmation"
-                type="email"
-                placeholder="Podaj numer telefonu"
-              />
-              <label
-                class="contact__form__fieldset__label-required"
-                for="message"
-                >Wiadomość</label
-              >
-              <textarea
-                class="contact__form__fieldset__input-textarea"
-                v-model="formData.message"
-                id="message"
-                name="message"
-                cols="50"
-                rows="7"
-                placeholder="Podaj treść wiadomości"
-                required
-              />
-              <input
-                class="contact__form__fieldset__input"
-                @click.prevent="sendEmail()"
-                type="submit"
-                name="submit"
-                value="Wyślij"
-              />
-            </fieldset>
-          </form>
-        </section>
-      </div>
+        <div class="contact">
+          <section>
+            <h3 class="text-center">Dane kontaktowe</h3>
+            <p>
+              Dziękujemy za zainteresowanie ofertą Rosco Serwis.
+              <br />Aby uzyskać więcej informacji na temat naszej firmy zapraszamy do kontaktu z nami codziennie od poniedziałku do piątku w godzinach od 7:00 do 15:00
+            </p>
+            <p
+              class="footer__contact"
+              v-for="(contact_detail, i) in $static.contact_data.contact_details"
+              :key="'contact-' + i"
+              v-html="contact_detail.contact"
+            ></p>
+          </section>
+          <section>
+            <form class="contact__form">
+              <h3 class="text-center">Napisz do nas!</h3>
+              <fieldset class="contact__form__fieldset">
+                <legend class="contact__form__fieldset__legend">Formularz kontaktowy</legend>
+                <label class="contact__form__fieldset__label-required" for="sender">Imię i nazwisko</label>
+                <input
+                  class="contact__form__fieldset__input"
+                  v-model="formData.sender"
+                  id="sender"
+                  name="sender"
+                  type="text"
+                  placeholder="Podaj imię i nazwisko"
+                  required
+                />
+                <label class="contact__form__fieldset__label-required" for="email">Adres e-mail</label>
+                <input
+                  class="contact__form__fieldset__input"
+                  v-model="formData.email"
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Podaj email"
+                  required
+                />
+                <label
+                  class="contact__form__fieldset__label-required"
+                  for="emailConfirmation"
+                >Potwierdź email</label>
+                <input
+                  class="contact__form__fieldset__input"
+                  v-model="formData.emailConfirmation"
+                  id="emailConfirmation"
+                  name="emailConfirmation"
+                  type="email"
+                  placeholder="Potwierdź email"
+                />
+                <label class="contact__form__fieldset__label-required" for="message">Wiadomość</label>
+                <textarea
+                  class="contact__form__fieldset__input-textarea"
+                  v-model="formData.message"
+                  id="message"
+                  name="message"
+                  cols="50"
+                  rows="7"
+                  placeholder="Podaj treść wiadomości"
+                  required
+                />
+                <input
+                  class="contact__form__fieldset__input"
+                  @click.prevent="sendEmail()"
+                  type="submit"
+                  name="submit"
+                  value="Wyślij"
+                />
+              </fieldset>
+            </form>
+          </section>
+        </div>
 
-      <!--<section v-html="$page.pageData.content"></section>-->
-    </article>
-  </main>
+        <modal v-if="showModal" @close="showModal = false">
+          <h4
+            slot="header"
+            :class="modalMessage.success ? 'modal__title-success' : 'modal__title-error' "
+          >{{modalMessage.title}}</h4>
+          <div slot="body" v-html="modalMessage.body"></div>
+        </modal>
+        <!--<section v-html="$page.pageData.content"></section>-->
+      </article>
+    </main>
+  </Layout>
 </template>
 
 <page-query>
@@ -118,35 +112,95 @@ query {
 </page-query>
 
 <script>
-const axios = require("axios");
+import Modal from "@/components/Modal.vue";
+
 export default {
   name: "About",
   metaInfo: {
     title: "O firmie",
   },
+  components: {
+    Modal,
+  },
   data() {
     return {
       formData: {
-        sender: "",
-        email: "",
-        emailConfirmation: "",
-        message: "",
+        sender: null,
+        email: null,
+        emailConfirmation: null,
+        message: null,
+        errors: [],
       },
+      showModal: false,
+      modalMessage: { title: "", body: "", success: false },
     };
   },
   methods: {
     async sendEmail() {
+      this.formData.errors = [];
+      this.modalMessage.title = "";
+      this.modalMessage.body = "";
+
+      if (!this.formData.sender) {
+        this.formData.errors.push(
+          "<li>Imię i nazwisko nie może być puste</li>"
+        );
+      }
+
+      if (!this.formData.email) {
+        this.formData.errors.push("<li>Email nie może być pusty</li>");
+      }
+
+      if (!this.formData.emailConfirmation) {
+        this.formData.errors.push(
+          "<li>Potwierdzenie email'a nie może być puste</li>"
+        );
+      }
+
+      if (!this.formData.message) {
+        this.formData.errors.push(
+          "<li>Treść wiadomości nie może być pusta</li>"
+        );
+      }
+
+      if (this.formData.email !== this.formData.emailConfirmation) {
+        this.formData.errors.push("<li>Adresy email muszą być takie same</li>");
+      }
+
+      if (this.formData.errors.length) {
+        this.showModal = true;
+        this.modalMessage.success = false;
+        this.modalMessage.title = "Błąd walidacji formularza";
+
+        this.modalMessage.body += "<ul>";
+
+        for (const errorMessage of this.formData.errors) {
+          this.modalMessage.body += errorMessage;
+        }
+
+        this.modalMessage.body += "</ul>";
+
+        return;
+      }
+
       try {
-          await axios.post("/.netlify/functions/sendgrid", {
+        await axios.post("/.netlify/functions/sendgrid", {
           emailSender: this.formData.email,
           senderName: this.formData.sender,
           message: this.formData.message,
         });
-        alert("Thank you, your message was sent successfully!");
+
+        this.modalMessage.success = true;
+        this.modalMessage.title = "Pomyślnie wysłano wiadomość";
+        this.modalMessage.body =
+          "<p>Wiadomość została dostarczona. Nasz zespół odpowie najszybciej jak tylko to możliwe.</p>";
       } catch (e) {
-        console.error(e);
-        alert("Error:  Your message could not be sent");
+        this.modalMessage.success = false;
+        this.modalMessage.title = "Nie udało się wysłać wiadomości";
+        this.modalMessage.body =
+          "<p>Nie udało się wysłać wiadomości, prosimy o kontakt przy użyciu innych metod.</p>";
       }
+      this.showModal = true;
     },
   },
 };
@@ -163,7 +217,7 @@ query {
 }
 </static-query>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .contact {
   display: flex;
   flex-direction: column;
@@ -214,6 +268,7 @@ query {
 
     &__fieldset {
       display: flex;
+      flex-direction: column;
       margin-inline-start: 2px;
       margin-inline-end: 2px;
       border: groove 2px ThreeDFace;
@@ -255,7 +310,7 @@ query {
         color: var(--title-color);
         background-color: var(--bg-color);
         padding: calc(var(--space) / 3);
-        transition: var(--transition-time) ease-in-out;
+        transition: box-shadow var(--transition-time) ease-in-out;
         outline: none;
         font-size: 0.8rem;
 
